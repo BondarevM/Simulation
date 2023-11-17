7304a476bea6b5f5794b0e5e96ee5bcb5ffd0faa
Mikhail <bondarevmisha99@gmail.com>
2023-11-17 23:28:57 +0700

    version 0.2

--- src/bondarev/Main.java
+++ src/bondarev/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Simulation simulation = new Simulation();
         Map map = new Map();
+        simulation.setupDefaultEntitiesPosition();
 
 
 
--- src/bondarev/Simulation.java
+++ src/bondarev/Simulation.java
@@ -1,20 +1,21 @@
 package bondarev;
+
 import bondarev.entities.*;
 
 
 public class Simulation {
-private final String  EMPTY_CELL = " - ";
-private final String PREDATOR = "X";
+    private final String EMPTY_CELL = " - ";
+    private final String PREDATOR = "X";
 
-    public void render(Simulation simulation){
-        for (int y = 10; y >= 0; y--){
+    public void render(Simulation simulation) {
+        for (int y = 10; y >= 0; y--) {
             String line = "";
-            for (int x = 0; x <= 40; x++){
-                Coordinates coordinates = new Coordinates();
+            for (int x = 0; x <= 40; x++) {
+                Coordinates coordinates = new Coordinates(x,y);
 
-                if(simulation.isCellEmpty(coordinates)) {
+                if (simulation.isCellEmpty(coordinates)) {
                     line += EMPTY_CELL;
-                } else{
+                } else {
                     line += getEntityImage(simulation.getEntity(coordinates));
                 }
             }
@@ -26,19 +27,27 @@ private final String PREDATOR = "X";
     private String getEntityImage(Entity entity) {
         return "X";
     }
-    public void setupDefaultEntitiesPosition(){
-        for(int y =)
+
+    public void setupDefaultEntitiesPosition() {
+        for (int y = 10; y >= 0; y--) {
+            for (int x = 0; x <= 40; x++) {
+                if ((x + y) % 5 == 4) {
+                    setEntity(new Coordinates(x,y), new Predator(new Coordinates(x,y), 5, 1,1)) ;
+                }
+            }
+        }
     }
 
-    public boolean isCellEmpty(Coordinates coordinates){
+    public boolean isCellEmpty(Coordinates coordinates) {
         return !Map.map.containsKey(coordinates);
     }
-    public Entity getEntity(Coordinates coordinates){
+
+    public Entity getEntity(Coordinates coordinates) {
         return Map.map.get(coordinates);
     }
 
-    public void setEntity(Coordinates coordinates, Entity entity){
+    public void setEntity(Coordinates coordinates, Entity entity) {
         entity.coordinates = coordinates;
-        Map.map.put(coordinates,entity);
+        Map.map.put(coordinates, entity);
     }
 }
